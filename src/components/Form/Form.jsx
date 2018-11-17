bf0520ddd40a96fbe92b803b7917958695fa577a
Kian <esmailpour.hosein@gmail.com>
2018-11-17 23:31:54 +0330

    Add props and style to Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Card, CardHeader, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 
 import './Form.scss';
 
 class Form extends Component {
   static defaultProps = {
-    classes: {}
+    classes: {},
+    title: '',
+    submitButtonText: 'Submit'
   };
 
   render() {
-    const { classes } = this.props;
+    const {
+      classes,
+      children,
+      title,
+      submitButtonText,
+      onSubmit
+    } = this.props;
 
     return (
-      <div className={`Form ${classes.root}`}>
-        Form
-      </div>
+      <form
+        className={`Form ${classes.root}`}
+        noValidate
+        autoComplete="off"
+      >
+        <Card className={classes.card}>
+          {title && <CardHeader title={title} />}
+          <CardContent>
+            {children}
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="outlined"
+              size="small"
+              color="primary"
+              onClick={onSubmit}
+            >
+              {submitButtonText}
+            </Button>
+          </CardActions>
+        </Card>
+      </form>
     );
   }
 }
 
 Form.propTypes = {
   classes: PropTypes.object,
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string,
+  submitButtonText: PropTypes.string,
+  onSubmit: PropTypes.func.isRequired
 };
 
 export default Form;
