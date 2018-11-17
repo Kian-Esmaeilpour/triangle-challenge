d75c26db953df590a82df9ee390c0364cae9c85d
Kian <esmailpour.hosein@gmail.com>
2018-11-17 19:59:02 +0330

    Add Main and Header components to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Main from '../Main';
+import Header from '../Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Main />
       </div>
     );
   }
