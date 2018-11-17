7e4cc970d3b422c901470032eed697f1c298a82c
Kian <esmailpour.hosein@gmail.com>
2018-11-17 19:42:47 +0330

    Add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Header.scss';
+
+class Header extends Component {
+  static defaultProps = {
+    classes: {}
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={`Header ${classes.root}`}>
+        Header
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  classes: PropTypes.object,
+};
+
+export default Header;
