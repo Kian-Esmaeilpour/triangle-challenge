75a8abd12d5401747b0231bba9538124b03fabb8
Kian <esmailpour.hosein@gmail.com>
2018-11-18 14:07:06 +0330

    Add side length inputs to triangle type determiner

--- src/components/TriangleTypeDeterminer/TriangleTypeDeterminer.jsx
+++ src/components/TriangleTypeDeterminer/TriangleTypeDeterminer.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import './TriangleTypeDeterminer.scss';
+import Form from '../Form';
+import SideLengthInput from '../SideLengthInput';
+import { setSideLength } from '../../actions/triangleActions';
 
 class TriangleTypeDeterminer extends Component {
   static defaultProps = {
@@ -9,18 +13,44 @@ class TriangleTypeDeterminer extends Component {
   };
 
   render() {
-    const { classes } = this.props;
+    const { classes, triangleSides, onSideLengthChange } = this.props;
 
     return (
       <div className={`TriangleTypeDeterminer ${classes.root}`}>
-        TriangleTypeDeterminer
+        <Form
+          title="Triangle Type Determiner"
+          onSubmit={onSideLengthChange}
+        >
+          {
+            triangleSides.map(side => (
+              <SideLengthInput
+                key={side.id}
+                label={side.id}
+                id={side.id}
+                onChange={onSideLengthChange}
+                value={side.value}
+              />
+            ))
+          }
+        </Form>
       </div>
     );
   }
 }
 
 TriangleTypeDeterminer.propTypes = {
-  classes: PropTypes.object,
+  classes: PropTypes.object
 };
 
-export default TriangleTypeDeterminer;
+const mapStateToProps = state => ({
+  triangleSides: state.triangle.triangleSides
+});
+
+const mapDispatchToProps = dispatch => ({
+  onSideLengthChange: id => event => dispatch(setSideLength(id, event.target.value))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TriangleTypeDeterminer);
