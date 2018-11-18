c1dfbf630b10fe8bb781d035541f2139efb30cc2
Kian <esmailpour.hosein@gmail.com>
2018-11-18 19:07:43 +0330

    Export validators separately

--- src/utils/triangleValidator.js
+++ src/utils/triangleValidator.js
@@ -3,7 +3,7 @@ import errorTypes from '../constants/errorTypes';
 const isSideLengthLessOrEqualZero = side => side.value <= 0;
 
 // Sum of each two sides  should be greater than the third side
-const isInvalidTriangleSides = sides => {
+export const isInvalidTriangleSides = sides => {
   return (
     sides[0].value + sides[1].value <= sides[2].value ||
     sides[0].value + sides[2].value <= sides[1].value ||
@@ -11,19 +11,18 @@ const isInvalidTriangleSides = sides => {
   );
 };
 
-const validateTriangle = sides => {
+export const validateTriangle = sides => {
   const errors = [];
   if (sides.some(isSideLengthLessOrEqualZero)) {
     errors.push(errorTypes.ZERO_OR_NEGATIVE_LENGTH);
   } else if (isInvalidTriangleSides) {
     errors.push(errorTypes.INVALID_TRIANGLE_SIDES);
   }
+  //TODO: Check for provided sides length count
   return errors;
 };
 
-const validateSide = side => {
+export const validateSide = side => {
   console.log('isSideLengthLessOrEqualZero(side)', isSideLengthLessOrEqualZero(side));
   return isSideLengthLessOrEqualZero(side) ? [errorTypes.ZERO_OR_NEGATIVE_LENGTH] : [];
 };
-
-export default { validateTriangle, validateSide };
