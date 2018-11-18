3d14c71265c4401bcd714a579cc51114dc34f1aa
Kian <esmailpour.hosein@gmail.com>
2018-11-18 13:59:05 +0330

    Add triangle action types

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,4 @@
+// Triangle constants
+export const SET_SIDE_LENGTH = 'SET_SIDE_LENGTH';
+export const SET_SIDE_ERRORS = 'SET_SIDE_ERRORS';
+export const SET_TRIANGLE_ERRORS = 'SET_TRIANGLE_ERRORS';
