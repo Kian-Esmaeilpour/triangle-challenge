68008df15f9c902329976dcaf4f2d337f53da645
Hosein (Kian) Esmailpour <esmailpour.hosein@gmail.com>
2018-11-19 16:49:51 +0330

    Update triangleValidator.test.js

--- src/utils/triangleValidator.test.js
+++ src/utils/triangleValidator.test.js
@@ -12,7 +12,7 @@ function getTriangleSides(lengthsArray) {
   return triangleSides.map((side, index) => ({ ...side, value: lengthsArray[index] }));
 }
 
-it('validate correct triangles', () => {
+it('validate valid triangles', () => {
   expect(validateTriangle(getTriangleSides([4, 4, 4]))).toHaveLength(0);
   expect(validateTriangle(getTriangleSides([1, 3, 3]))).toHaveLength(0);
   expect(validateTriangle(getTriangleSides([4, 5, 3]))).toHaveLength(0);
@@ -20,7 +20,7 @@ it('validate correct triangles', () => {
   expect(validateTriangle(getTriangleSides([2000, 2000, 2000]))).toHaveLength(0);
 });
 
-it('validate invalid triangles', () => {
+it('invalidate invalid triangles', () => {
   expect(validateTriangle(getTriangleSides([10, 5, 5]))).toContain(errorTypes.INVALID_TRIANGLE_SIDES);
   expect(validateTriangle(getTriangleSides([1, 12, 2]))).toContain(errorTypes.INVALID_TRIANGLE_SIDES);
   expect(validateTriangle(getTriangleSides([0, 0, 0]))).toContain(errorTypes.FORM_ZERO_OR_NEGATIVE_LENGTH);
@@ -32,7 +32,7 @@ it('validate invalid triangles', () => {
     .toEqual(expect.arrayContaining([errorTypes.FORM_EMPTY_LENGTH, errorTypes.FORM_ZERO_OR_NEGATIVE_LENGTH]));
 });
 
-it('validate triangle side', () => {
+it('triangle side validation', () => {
   const triangleSideSamp1 = getTriangleSides(['', 0, 5]);
   const triangleSideSamp2 = getTriangleSides([+1, -5, 50000]);
   expect(validateSide(triangleSideSamp1[0])).toContain(errorTypes.INPUT_EMPTY_LENGTH);
